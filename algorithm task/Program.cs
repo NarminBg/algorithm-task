33bb1906d2de7e8c022ab1fc42494b0b556e0d2f
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-28 19:00:48 +0400

    Add project files.

---
+++ algorithm task/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace algorithm_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int[] digits = new int[10];
+            bool muxtelifreqemli = true;
+            while (number > 0)
+            {
+
+                int digit = number % 10;
+                if (digits[digit] == 1)
+                {
+                    muxtelifreqemli = false;
+                    break;
+                }
+                digits[digit] = 1;
+                number /= 10;
+
+            }
+
+            if (muxtelifreqemli == true)
+            {
+                Console.WriteLine("muxtelifreqemli");
+            }
+        }
+    }
+}
